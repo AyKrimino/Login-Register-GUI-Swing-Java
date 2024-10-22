7f8ae31527dde4a46a3eee451e9f6a85fbef08d8
AyKrimino <ayoub.krimi14@gmail.com>
2024-10-22 14:01:25 +0100

    Creating the Register GUI

--- src/App.java
+++ src/App.java
@@ -5,7 +5,8 @@ public class App {
         SwingUtilities.invokeLater(new Runnable() { // ensures that the GUI is created and managed on the Event Dispatch Thread, which is essential for thread safety in Swing.
             @Override
             public void run() {
-                new LoginGUI().setVisible(true);
+                //new LoginGUI().setVisible(true);
+                new RegisterGUI().setVisible(true);
             }
         });
     }
---
+++ src/RegisterGUI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class RegisterGUI  extends JFrame {
+    public RegisterGUI() {
+        super("Register");
+        setSize(CommonConstants.FRAME_SIZE);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setLayout(null);
+    }
+}
